ab4e579138d3be234db0e8f9727aba8e1102d5a8
unknown <madysoftware@gmail.com>
2020-03-28 19:39:38 +0500

    class work

---
+++ game/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string task = "Что в огне не горит и в воде не тонет?";
+            string answer = "Лёд";
+            int tryCount = 5;
+
+            Console.WriteLine("Привет, давай сыграем в игру!\nОтгадай мою загадку! :-)");
+            Console.WriteLine(task);
+            Console.WriteLine("У тебя 5 попыток! Дерзай детка!"); 
+            for(int i = 0; i < 5; i++)
+            {
+                string inputedWord = Console.ReadLine().ToLower();
+                if(inputedWord == answer.ToLower())
+                    break;
+                else
+                {
+                    Console.WriteLine($"У вас осталось {--tryCount} попыток!");
+                    continue;                
+                }
+            }
+            if(tryCount == 0)
+                Console.WriteLine("Ты проиграл");
+            else
+                Console.WriteLine("Победа");        
+        }
+    }
+}
---
+++ taskFor7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace taskFor7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 5, B = 11, c = 0;
+            for(int i = A; i <= B; i++)
+            {
+                c += i;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
